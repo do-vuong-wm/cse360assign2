3d68a7416e1959c4d47517a2e403e5264aa93303
Vuong Do <vuongdotf2@gmail.com>
2019-10-05 15:33:30 -0700

    Initial commit.

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,43 @@
+/**
+ * Name: Vuong Do
+ * Class ID: CSE360
+ * Assignment #: 2
+ * 
+ * Contains AddingMachine class
+ */
+package cse360assign2;
+
+/**
+ * AddingMachine Class
+ * 
+ * @author Vuong
+ *
+ */
+public class AddingMachine {
+
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	public int getTotal () {
+		return 0;
+	}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+		
+	public String toString () {
+		return "";
+	}
+
+	public void clear() {
+	
+	}
+}
